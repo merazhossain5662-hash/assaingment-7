d0c05889d000918658d22ff8bd463978ca601b8e
merazhossain5662-hash <merazhossain5662@gmail.com>
2026-04-17 21:12:35 +0600

    Added loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,17 @@
+
+import { GridLoader, RingLoader } from 'react-spinners';
+
+const loading = () => {
+    return (
+        <div className='w-10/12 mxa
+        '>
+           <GridLoader
+  color="green"
+  size={27}
+  margin={24}
+/>
+        </div>
+    );
+};
+
+export default loading;
